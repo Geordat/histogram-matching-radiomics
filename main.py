0ec4b75cd763638139ea023705708b627267d540
Geordat <58336111+Geordat@users.noreply.github.com>
2021-07-08 10:43:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,72 +8,25 @@ import os
 
 def main():
 
-    ######################################################################################################
-    ######################################### PREPROCESS DATASET #########################################
+    DATASET_PATH = os.path.join('data', 'dataset', 'sygrisampol_images')
+
+    post_contrast_imgs = glob.glob(os.path.join(DATASET_PATH, "*post-contrast.tif_removed_background.png"))
     
-    # Every Image has 3 channels (pre-contrast, FLAIR, post-contrast) and one mask
-    # Import and Split the channels to different folders 
-    # It creates new images increasing time and resources consumption
-    utils.split_dataset(DATASET_PATH)
-
-    # utils.merge_slices_into_3D_image(DATASET_PATH, 'pre-contrast')
-
-    # Prepare dataset for pyradiomics extractor
-    # Getting the dataset's path, returns an object specifing for each patient the images and segmentations
-    pre_contrast_dataset = utils.get_dataset_as_object(DATASET_PATH, 'pre-contrast') # pre-contrast-3D
-    flair_dataset = utils.get_dataset_as_object(DATASET_PATH, 'flair')
-    post_contrast_dataset = utils.get_dataset_as_object(DATASET_PATH, 'post-contrast')
-
-
-    ######################################################################################################
-    ##################################### EXTRACT RADIOMICS FEATURES #####################################
-
-    # Initialize PyRadiomics feature extractor wrapper
-    feature_extractor = FeatureExtractor(PARAMETERS_PATH)
+    h = [cv2.imread(x, 0)   for x in post_contrast_imgs]
+  
+    
+    clip_lim = [5 ,20 , 40 ]
+    images = [h,h,h]
+    p=0
+    for i in clip_lim:
+        images[p] = utils.histogram_equalization_CLAHE(post_contrast_imgs,tile_grid_size=(24,24), clip_limit=i)
+        utils.histograms_compare(images[p],post_contrast_imgs,name=i)
+        p=p+1
         
-    # Execute batch processing to extract features
-    feature_extractor.extract_features(post_contrast_dataset, FEATURES_OUTPUT_PATH)
-
-    # Get the filepaths from the images only (without the segmentations) as a list
-    pre_contrast_images = [value['Image'] for value in pre_contrast_dataset.values()]
-    flair_images = [value['Image'] for value in flair_dataset.values()]
-    post_contrast_images = [value['Image'] for value in post_contrast_dataset.values()]
-
-
-    # ######################################################################################################
-    # ######################################### HISTOGRAM MATCHING #########################################
-
-    # Initialize HistogramMatcher & select histogram matching method
-    histogram_matcher = HistogramMatcher(NEW_DATASET_OUTPUT_PATH, 'ExactHistogramMatching')
-
-    # Perform histogram matching
-    # histogram_matcher.match_histograms(flair_images[0], flair_images[1], display=True)
-
-    # Perform Batch histogram matching
-    histogram_matcher.match_histograms(flair_images, flair_images[4], display=True) # TODO: dataset[0] is temporal, should we automate reference image selection?
-
-
-    ######################################################################################################
-    ##################################### EXTRACT RADIOMICS FEATURES #####################################
-    #####################################    FROM THE NEW DATASET    #####################################
-
-    # # Import the dataset
-    # new_dataset = utils.get_dataset_as_object(NEW_DATASET_OUTPUT_PATH, 'flair')
-
-    # # We don't have the segmentations on the new dataset folder because we created it in the previous step
-    # # by applying histogram matching on the image, not the segmentation.
-    # # So we need to copy the segmentations paths from the old dataset and add them to the new dataset's dictionary
-    # new_dataset = utils.insert_segmenetions_path_to_dict(new_dataset, NEW_DATASET_OUTPUT_PATH, DATASET_PATH, 'flair')
-
-    # # Execute batch processing to extract features
-    # feature_extractor.extract_features(new_dataset, NEW_FEATURES_OUTPUT_PATH)
-
-    # # Get the filepaths from the images only (without the segmentations)
-    # new_dataset = [value['Image'] for value in new_dataset.values()]
-
+    utils.ssim_compare(h,images,post_contrast_imgs)
     
-    ######################################################################################################
-    ########################################### COMPARE RESULTS ##########################################
+    CLAHE_images_final = images[0]
+
 
     
 
