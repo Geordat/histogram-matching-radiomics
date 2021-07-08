aff082645b35f1b0e248c19f9a0a53ea121d7608
Geordat <58336111+Geordat@users.noreply.github.com>
2021-07-08 12:40:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,35 @@ import os
 
 
 def main():
-
+    
     DATASET_PATH = os.path.join('data', 'dataset', 'sygrisampol_images')
 
     post_contrast_imgs = glob.glob(os.path.join(DATASET_PATH, "*post-contrast.tif_removed_background.png"))
     
-    h = [cv2.imread(x, 0)   for x in post_contrast_imgs]
-  
     
+    reference_img=  cv2.imread(post_contrast_imgs[0],0)
+    target_img  = cv2.imread(post_contrast_imgs[5],0)
+    
+    
+    #exact histogram matching
+    
+    exact_matching_final = utils.exact_histogram_matching(post_contrast_imgs,post_contrast_imgs[0])
+    
+    # CLAHE 
+    
+    init_img = [cv2.imread(x, 0)   for x in post_contrast_imgs]
     clip_lim = [5 ,20 , 40 ]
-    images = [h,h,h]
+    images = [[],[],[]]
     p=0
     for i in clip_lim:
         images[p] = utils.histogram_equalization_CLAHE(post_contrast_imgs,tile_grid_size=(24,24), clip_limit=i)
         utils.histograms_compare(images[p],post_contrast_imgs,name=i)
         p=p+1
         
-    utils.ssim_compare(h,images,post_contrast_imgs)
+    utils.ssim_compare(init_img,images,post_contrast_imgs)
     
     CLAHE_images_final = images[0]
-
+    
 
     
 
